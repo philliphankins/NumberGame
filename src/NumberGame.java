607a714e0395d75441b5794a652a31531beb646a
philliphankins <34249287+philliphankins@users.noreply.github.com>
2018-01-15 13:35:07 -0700

    int randomNumber and test.

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -1,6 +1,10 @@
+import.java.util.Scanner
+
 public class NumberGame {
 
     public static void main(String[] args) {
-        System.out.println("Testola!");
+        int randomNumber = (int) Math.random * 100 + 1;
+        System.out.println("I have randomly chose a number between 1 and 100.");
+        System.out.println("Try to guess it.");
+        System.out.println(randomNumber);
     }
-}
